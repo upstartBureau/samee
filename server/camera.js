52111f40d189cec41bfc852035b6d7eac5b0db3b
Jake Mensch <jake.mensch@gmail.com>
2017-09-07 07:22:21 -0700

    camera-on and camera-off endpoints working

---
+++ server/camera.js
@@ -0,0 +1,66 @@
+
+/////////////////// IMPORTS //////////////////////
+
+const SSH = require('simple-ssh'),
+      Promise = require('bluebird');
+
+/////////////////// CONFIG ///////////////////////
+
+const SSH_HOST    = 'arduino.local',
+      SSH_USER    = 'root',
+      SSH_PASS    = 'arduino',
+      STREAM_PORT = '8080';
+
+//////////////// INITIALIZATION //////////////////
+
+const ssh = new SSH({
+  host: SSH_HOST,
+  user: SSH_USER,
+  pass: SSH_PASS
+});
+
+ssh.on('error', err => console.log("SSH ERROR:", err));
+
+////////////// PRIVATE FUNCTIONS /////////////////
+
+function execSSH(cmd) {
+  return new Promise((resolve, reject) => {
+    ssh.exec(cmd, {
+      exit: (code, stdout, stderr) => {
+        ssh.reset(() => resolve({code, stdout, stderr}))
+      }
+    }).start();
+  });
+}
+
+function cameraOn() {
+  let cmd = `mjpg_streamer -i 'input_uvc.so -d /dev/video0 -r 360x600 -f 25' ` +
+            `-o 'output_http.so -p ${STREAM_PORT} -w /www/webcam' &`;
+
+  return execSSH(cmd)
+    .then(({code, stdout, stderr}) => {
+      if (code === 0)
+        return Promise.resolve('camera on');
+      else
+        return Promise.reject(stderr);
+    });
+}
+
+function cameraOff() {
+  let cmd = 'killall webcamDaemon mjpg_streamer';
+
+  return execSSH(cmd)
+    .then(({code, stdout, stderr}) => {
+      if (code === 1)
+        return Promise.resolve('camera off');
+      else
+        return Promise.reject(stderr);
+    });
+}
+
+//////////////////// EXPORTS //////////////////////
+
+module.exports = {
+  on:  cameraOn,
+  off: cameraOff
+};
--- server/index.js
+++ server/index.js
@@ -3,19 +3,40 @@
 
 const http = require('http'),
       url = require('url'),
-      { exec } = require('child_process');
+      { exec } = require('child_process'),
+      camera = require('./camera');
 
 //////////////////// FUNCTIONS /////////////////////
 
+function serveResponse(response, code, payload) {
+  response.writeHead(code);
+  response.end(payload);
+}
+
+function handleRequest(action, response) {
+  action
+    .then(data => serveResponse(response, 200, data))
+    .catch(err => serveResponse(response, 500, err));
+}
+
 function server(request, response) {
   let urlInfo = url.parse(request.url, true);
+  console.log("Received:", urlInfo.href);
+
   switch(urlInfo.pathname) {
+    case '/on':
+      handleRequest(camera.on(), response);
+      break;
+    case '/off':
+      handleRequest(camera.off(), response);
+      break;
     case '/say':
-      console.log("received say command");
-      console.log(urlInfo.query);
-      exec('say ' + urlInfo.query.statement);
-      response.writeHead(200);
-      response.end('hello');
+      let { words } = urlInfo.query;
+      exec('say ' + words);
+      serveResponse(response, 200, 'said ' + words);
+      break;
+    default:
+      serveResponse(response, 404, 'endpoint does not exist');
       break;
   }
 }
